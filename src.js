e831e21affc68e4ef8feedf0622bfd11b0827b9c
Hoang-Dang173 <suonghung2005@gmail.com>
2025-02-10 00:11:06 +1100

    rearranging files and add comments

--- src.js
+++ src.js
@@ -1,120 +1,116 @@
 
+//All codes below are Jquery syntaxed. 
 
-    var counter = 1;
-    var face_link; 
-    $('body').on('click', '.next', function() { 
-        $('.container').hide();
+var counter = 1;
+var face_link; 
+//if button next is clicked,
+$('body').on('click', '.next', function() { 
+    $('.container').hide();
+    //increase counter per click
+    counter++;
+    if (counter <= 9) {
+        // show corresponding pages based on counter
+        $('#content-'+counter+'').show();
+    }
     
-        counter++;
-        if (counter <= 9) {
-            $('#content-'+counter+'').show();
-        }
-        
-        if(counter > 1) {
-            $('.prev').show();
-        };
-        setStyle(counter); 
-        if(counter == 10) {
-            $('.next').hide(); 
-            $('#end').show(); 
-        };
-        
-    });
+    if(counter > 1) {
+        // show button prev from page 1 onwards
+        $('.prev').show();
+    };
+    // set styles for each page 
+    setStyle(counter); 
+    if(counter == 10) {
+        // 'next' button is hidden in last page and show end div. 
+        $('.next').hide(); 
+        $('#end').show(); 
+    };
     
-    $('body').on('click', '.prev', function() { 
-        //alert(counter);
-        counter--;
-        $('.container').hide();
-        var id = counter;    
-        $('#content-'+id).show();
-        
-        setStyle(counter); 
+});
 
-        if(counter <= 9) {
-            $('.next').show();
-        };
-
-        if(counter ==  1){
-            $('.prev').hide();
-        }
-    });
+// prev button effect: symmetrical to when the next button is clicked. 
+$('body').on('click', '.prev', function() { 
+    counter--;
+    $('.container').hide();
+    var id = counter;    
+    $('#content-'+id).show();
     
-    $('body').on('click', '.edit-previous', function() { 
-        $('.end').hide();
-        $('.content-holder').show();
-        $('#content-3').show();
-    });
+    setStyle(counter); 
 
+    if(counter <= 9) {
+        $('.next').show();
+    };
 
+    if(counter ==  1){
+        $('.prev').hide();
+    }
+});
 
-
-    function setStyle (counter) {
-        if (counter == 1) {
-            document.body.style.backgroundImage = "url('Images/greeting_face.png'), url('Images/star_pattern.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = "370px 0px, center";
-            document.body.style.backgroundSize = "900px 700px, auto";
-            document.body.style.backgroundAttachment = "fixed";
-
-        }
-        else if (counter == 2) {
-            document.body.style.backgroundImage = "url('Images/about.png'), url('Images/star_pattern.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = "370px 0px, center";
-            document.body.style.backgroundSize = "900px 700px, auto";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if (counter == 3 || counter == 4) {
-            document.body.style.backgroundImage = "url('Images/about.png'), url('Images/hobbies.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = "370px 0px, fixed";
-            document.body.style.backgroundSize = "900px 700px";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if ( counter == 5) {
-            document.body.style.backgroundImage = "url('Images/h_i_face.png'), url('Images/hobbies.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px, fixed";
-            document.body.style.backgroundSize = "900px 700px";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if (counter == 6) {
-            document.body.style.backgroundImage = "url('Images/h_i_face.png'), url('Images/math_back.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px";
-            document.body.style.backgroundSize = "900px 700px, cover";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if (counter == 7) {
-            document.body.style.backgroundImage = "url('Images/project_face.png'), url('Images/math_back.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px";
-            document.body.style.backgroundSize = "900px 700px, cover";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if (counter == 8) {
-            document.body.style.backgroundImage = "url('Images/project_cry.png'), url('Images/recursive1.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px";
-            document.body.style.backgroundSize = "900px 700px, auto";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else if (counter == 9) {
-            document.body.style.backgroundImage = "url('Images/links_face.png'), url('Images/recursive1.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px";
-            document.body.style.backgroundSize = "900px 700px, auto";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-        else {
-            document.body.style.backgroundImage = "url('Images/bye_face.png'), url('Images/recursive1.jpg')";
-            document.body.style.backgroundRepeat = "no-repeat, repeat";
-            document.body.style.backgroundPosition = " 370px 0px";
-            document.body.style.backgroundSize = "900px 700px, auto";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-
-       
+// Set style for each click - mainly background details
+// Alternatively another function could be used to avoid repetitions but this way makes the designing process more flexible. 
+function setStyle (counter) {
+    if (counter == 1) {
+        document.body.style.backgroundImage = "url('Images/face_background/greeting_face.png'), url('Images/Background/star_pattern.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = "370px 0px, center";
+        document.body.style.backgroundSize = "900px 700px, auto";
+        document.body.style.backgroundAttachment = "fixed";
 
     }
+    else if (counter == 2) {
+        document.body.style.backgroundImage = "url('Images/face_background/about.png'), url('Images/Background/star_pattern.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = "370px 0px, center";
+        document.body.style.backgroundSize = "900px 700px, auto";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if (counter == 3 || counter == 4) {
+        document.body.style.backgroundImage = "url('Images/face_background/about.png'), url('Images/Background/hobbies.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = "370px 0px, fixed";
+        document.body.style.backgroundSize = "900px 700px";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if ( counter == 5) {
+        document.body.style.backgroundImage = "url('Images/face_background/h_i_face.png'), url('Images/Background/hobbies.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px, fixed";
+        document.body.style.backgroundSize = "900px 700px";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if (counter == 6) {
+        document.body.style.backgroundImage = "url('Images/face_background/h_i_face.png'), url('Images/Background/math_back.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px";
+        document.body.style.backgroundSize = "900px 700px, cover";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if (counter == 7) {
+        document.body.style.backgroundImage = "url('Images/face_background/project_face.png'), url('Images/Background/math_back.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px";
+        document.body.style.backgroundSize = "900px 700px, cover";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if (counter == 8) {
+        document.body.style.backgroundImage = "url('Images/face_background/project_cry.png'), url('Images/Background/recursive1.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px";
+        document.body.style.backgroundSize = "900px 700px, auto";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else if (counter == 9) {
+        document.body.style.backgroundImage = "url('Images/face_background/links_face.png'), url('Images/Background/recursive1.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px";
+        document.body.style.backgroundSize = "900px 700px, auto";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+    else {
+        document.body.style.backgroundImage = "url('Images/face_background/bye_face.png'), url('Images/Background/recursive1.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat, repeat";
+        document.body.style.backgroundPosition = " 370px 0px";
+        document.body.style.backgroundSize = "900px 700px, auto";
+        document.body.style.backgroundAttachment = "fixed";
+    }
+}
 
